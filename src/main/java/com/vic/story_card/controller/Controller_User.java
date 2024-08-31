311f4cb9e084efc28da170163798044b8dcd0d62
vicLim88 <120290960+vicLim88@users.noreply.github.com>
2024-08-31 15:48:16 +0800

    added users page

---
+++ src/main/java/com/vic/story_card/controller/Controller_User.java
@@ -0,0 +1,19 @@
+package com.vic.story_card.controller;
+
+import com.vic.story_card.model.Model_User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Controller_User {
+    @GetMapping("user_names")
+    public String user_names(Model model) {
+        Model_User user = new Model_User("Vic",
+                "vic.lim@icloud.com",
+                "ADMIN",
+                "MALE");
+        model.addAttribute("user", user);
+        return "user_names";
+    }
+}
---
+++ src/main/java/com/vic/story_card/model/Model_User.java
@@ -0,0 +1,48 @@
+package com.vic.story_card.model;
+
+public class Model_User {
+    private String user_name;
+    private String user_email;
+    private String user_role;
+    private String user_gender;
+
+    public Model_User(String user_name, String user_email, String user_role, String user_gender) {
+        this.user_name = user_name;
+        this.user_email = user_email;
+        this.user_role = user_role;
+        this.user_gender = user_gender;
+    }
+
+    public String getUser_name() {
+        return user_name;
+    }
+
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
+    }
+
+    public String getUser_email() {
+        return user_email;
+    }
+
+    public void setUser_email(String user_email) {
+        this.user_email = user_email;
+    }
+
+    public String getUser_role() {
+        return user_role;
+    }
+
+    public void setUser_role(String user_role) {
+        this.user_role = user_role;
+    }
+
+    public String getUser_gender() {
+        return user_gender;
+    }
+
+    public void setUser_gender(String user_gender) {
+        this.user_gender = user_gender;
+    }
+
+}
